9192eea81ed9a83438eae5cb7f15b0ae44d26521
Fohim Neusburg <75149703+fohimneusburg@users.noreply.github.com>
2022-07-10 14:18:45 +0100

    Create IndividualSavingsAccount-003.sol

---
+++ IndividualSavingsAccount-003.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+import"./SafeMaths.sol";
+
+contract IndividualSavingsAccount {
+
+    using SafeMath for uint256;
+
+    uint256 internal balance;
+    uint256 public time_locked;
+
+    address public owner;
+
+    modifier OnlyOwner() {
+        require (msg.sender == owner);
+        _;
+    }
+
+    modifier Locked() {
+        if (time_locked >= block.timestamp) {
+            revert();
+        }
+        _;
+    }
+
+    constructor (uint256 _timeLocked) payable {
+        balance = msg.value;
+        time_locked = _timeLocked;
+        owner = msg.sender;
+    }
+    
+    function Deposit() public payable OnlyOwner() {
+        require (msg.value > 0);
+        balance = balance.add(msg.value);
+    }
+     
+    function Withdraw(uint256 _amount) public OnlyOwner() Locked() {
+        payable(owner).transfer(_amount);
+        balance = balance.sub(_amount); 
+    }
+
+    function GetBalance() public view OnlyOwner() returns (uint256) {
+        return balance;
+    }
+
+    function LockForLonger(uint256 _timeLocked) public OnlyOwner() {
+        require (_timeLocked > time_locked);
+        time_locked = _timeLocked;
+    }
+
+    function ChangeOwner(address _newOwner) public OnlyOwner() {
+        owner = _newOwner;
+    }
+
+}
